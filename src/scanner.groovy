290f1bab28977b78fd85a07f06a8b9b16ac1bd80
Thierry Lacour <thi@praqma.net>
2016-03-03 10:50:26 +0100

    Simplistic implementation of group support

--- src/scanner.groovy
+++ src/scanner.groovy
@@ -1,27 +1,26 @@
 @Grab(group = 'org.yaml', module = 'snakeyaml', version = '1.17')
 import org.yaml.snakeyaml.Yaml
-
-import java.util.regex.Matcher
+@Grab(group = 'org.yaml', module = 'snakeyaml', version = '1.17')
+import org.yaml.snakeyaml.Yaml
 
 doArgsCheck()
 def rules = loadRules()
 
 def resultIsFailed = false
-def targetFiles = args[1..-1].collect{new File(it)}
+def targetFiles = args[1..-1].collect { new File(it) }
 targetFiles.each { file ->
     println "[FILE: $file]"
-    def text = file.text
     rules.each { rule ->
         println "[RULE: $rule.title]"
-        def matcher = text =~ rule.match
-        if(triggers(rule, matcher)){
-            if(rule.fail) resultIsFailed = true
-            println expandMessage(matcher, rule)
+        def matches = file.text.findAll(rule.match)
+        if (triggers(rule, matches)) {
+            if (rule.fail) resultIsFailed = true
+            println expandMessage(rule, matches)
         }
     }
 }
 
-if(resultIsFailed)
+if (resultIsFailed)
     System.exit(1)
 
 // Helper methods
@@ -30,42 +29,38 @@ if(resultIsFailed)
 private void doArgsCheck() {
     if (!args || args.size() < 2) {
         println "ERROR: Missing file arguments"
-        println "Usage:"
-        println "scanner [config] [targets]"
+        println "Usage: scanner [config] [targets]"
         System.exit(1)
     }
 }
 
 private ArrayList loadRules() {
     def rulesFile = new File(args[0])
-    def rules = new Yaml().load(rulesFile.text).collect {[
+    def rules = new Yaml().load(rulesFile.text).collect {
+        [
                 title  : it.key,
                 match  : ~it.value.match,
                 type   : it.value.type,
                 fail   : it.value.fail,
                 message: it.value.message
-    ]}
+        ]
+    }
     return rules
 }
 
-boolean triggers(def rule, Matcher matcher) {
-    println "[$matcher.count " + (matcher.count == 1 ? "match" : "matches") + "]"
-    def foundOnFind = matcher.count > 0 && rule.type.equalsIgnoreCase("onFind")
-    def missingOnMissing = matcher.count == 0 && rule.type.equalsIgnoreCase("onMissing")
+boolean triggers(def rule, def matches) {
+    println "[$matches.size " + (matches.size == 1 ? "match" : "matches") + "]"
+    def foundOnFind = matches.size > 0 && rule.type.equalsIgnoreCase("onFind")
+    def missingOnMissing = matches.size == 0 && rule.type.equalsIgnoreCase("onMissing")
     return foundOnFind || missingOnMissing
 }
 
-String expandMessage(Matcher originalMatcher, def rule) {
+String expandMessage(def rule, def matches) {
     def message = rule.message
-    def replacerMatcher = message =~ /(?<![$])(\$[1-9]\d*)/
-    if(!replacerMatcher.count) return message;
-
-    for (int m = 1; m <= replacerMatcher.count; m++) {
-        if (originalMatcher.count < m) break;
-        println originalMatcher.groupCount()
-        println originalMatcher.group(0)
-        def groupMatcher = message =~ /(?<![$])(\$${m}(?!\d))/
-        message = groupMatcher.replaceFirst(originalMatcher.group(m))
+    def groups = message =~ /(?<![$])(\$[1-9]\d*)/
+    if (!groups.count) return message;
+    for (int m = 1; m <= groups.count && m <= matches.size; m++) {
+        message = rule.message.replaceAll(/(?<![$])(\$${m}(?!\d))/, matches[m - 1])
     }
     return message
 }
